1dfa12c7513d25e822bb157723a69d2d6ce0c7a7
Jackson Fung <jacksonf21@hotmail.com>
2019-08-21 13:52:12 +0000

    add queries

---
+++ 1_queries/3_property_listing_city.sql
@@ -0,0 +1,9 @@
+SELECT properties.*, avg(rating) AS average_rating 
+  FROM properties
+  JOIN property_reviews
+  ON properties.id = property_id
+  WHERE city LIKE '%ancouver'
+  GROUP BY properties.id
+  HAVING avg(rating) >= 4
+  ORDER BY cost_per_night
+  LIMIT 10;
